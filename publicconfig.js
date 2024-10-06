5b32efff301dabbce658e2cfd650495b24ee941f
Eboxsl <Eboxsl@hotmail.com>
2024-10-06 06:30:58 +0530

    Update publicconfig.js

--- publicconfig.js
+++ publicconfig.js
@@ -1,4 +1,4 @@
 {
-  "hi": "hi",
-  "Nethindu": "❤️ The developer \n > 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺"
+  "Nethindu": "❤️ The developer \n > 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺",
+  "ALIVE_IMG":"https://telegra.ph/file/261c717e5e4f165712ec4.png"
 }
